9871459d816fe86d3ccb7fc8a6f3d5e23b104921
mrredacted <mrredacted@pop-os.localdomain>
2023-01-10 19:59:25 -0500

    Updated packages list, and fixed issue with format filtering for YTDL causing no stream to be returned

--- music/subscription.js
+++ music/subscription.js
@@ -1,113 +1,137 @@
 const {
-	AudioPlayerStatus,
-	createAudioPlayer,
-	entersState,
-	VoiceConnectionDisconnectReason,
-	VoiceConnectionStatus,
-} = require('@discordjs/voice');
-const { promisify } = require('util');
+  AudioPlayerStatus,
+  createAudioPlayer,
+  entersState,
+  VoiceConnectionDisconnectReason,
+  VoiceConnectionStatus,
+} = require("@discordjs/voice");
+const { promisify } = require("util");
 
 const wait = promisify(setTimeout);
 
 module.exports = class MusicSubscription {
-	constructor(voiceConnection, subscriptions, guildId) {
-		this.voiceConnection = voiceConnection;
-		this.subscriptions = subscriptions;
-		this.guildId = guildId;
-		this.audioPlayer = createAudioPlayer();
-		this.queue = [];
-		this.lastPlayed = {};
-		this.timeoutID;
+  constructor(voiceConnection, subscriptions, guildId) {
+    this.voiceConnection = voiceConnection;
+    this.subscriptions = subscriptions;
+    this.guildId = guildId;
+    this.audioPlayer = createAudioPlayer();
+    this.queue = [];
+    this.lastPlayed = {};
+    this.timeoutID;
 
-		this.voiceConnection.on('stateChange', async (oldState, newState) => {
-			if (newState.status === VoiceConnectionStatus.Disconnected) {
-				if (newState.reason === VoiceConnectionDisconnectReason.WebSocketClose && newState.closeCode === 4014) {
-					try {
-						await entersState(this.voiceConnection, VoiceConnectionStatus.Connecting, 5_000);
-					} catch {
-						this.voiceConnection.destroy();
-					}
-				} else if (this.voiceConnection.rejoinAttempts < 5) {
-					await wait((this.voiceConnection.rejoinAttempts + 1) * 5_000);
-					this.voiceConnection.rejoin();
-				} else {
-					this.voiceConnection.destroy();
-				}
-			} else if (newState.status === VoiceConnectionStatus.Destroyed) {
-				this.stop
-			} else if (
-				!this.readyLock &&
-				(newState.status === VoiceConnectionStatus.Connecting || newState.status === VoiceConnectionStatus.Signalling)
-			) {
-				this.readyLock = true;
-				try {
-					await entersState(this.voiceConnection, VoiceConnectionStatus.Ready, 20_000);
-				} catch {
-					if (this.voiceConnection.state.status !== VoiceConnectionStatus.Destroyed) this.VoiceConnection.destroy();
-				} finally {
-					this.readyLock = false;
-				}
-			}
-		});
+    this.voiceConnection.on("stateChange", async (oldState, newState) => {
+      if (newState.status === VoiceConnectionStatus.Disconnected) {
+        if (
+          newState.reason === VoiceConnectionDisconnectReason.WebSocketClose &&
+          newState.closeCode === 4014
+        ) {
+          try {
+            await entersState(
+              this.voiceConnection,
+              VoiceConnectionStatus.Connecting,
+              5_000
+            );
+          } catch {
+            this.voiceConnection.destroy();
+          }
+        } else if (this.voiceConnection.rejoinAttempts < 5) {
+          await wait((this.voiceConnection.rejoinAttempts + 1) * 5_000);
+          this.voiceConnection.rejoin();
+        } else {
+          this.voiceConnection.destroy();
+        }
+      } else if (newState.status === VoiceConnectionStatus.Destroyed) {
+        this.stop;
+      } else if (
+        !this.readyLock &&
+        (newState.status === VoiceConnectionStatus.Connecting ||
+          newState.status === VoiceConnectionStatus.Signalling)
+      ) {
+        this.readyLock = true;
+        try {
+          await entersState(
+            this.voiceConnection,
+            VoiceConnectionStatus.Ready,
+            20_000
+          );
+        } catch {
+          if (
+            this.voiceConnection.state.status !==
+            VoiceConnectionStatus.Destroyed
+          )
+            this.VoiceConnection.destroy();
+        } finally {
+          this.readyLock = false;
+        }
+      }
+    });
 
-		this.audioPlayer.on('stateChange', (oldState, newState) => {
-			if (newState.status === AudioPlayerStatus.Idle && oldState.status !== AudioPlayerStatus.Idle) {
-				console.log('Finished Playing!');
-				this.lastPlayed = oldState.resource.metadata;
-				oldState.resource.metadata.onFinish();
-				this.processQueue();
-			} else if (newState.status === AudioPlayerStatus.Playing) {
-				newState.resource.metadata.onStart();
-				console.log('Playing!');
-			}
-		});
+    this.audioPlayer.on("stateChange", (oldState, newState) => {
+      if (
+        newState.status === AudioPlayerStatus.Idle &&
+        oldState.status !== AudioPlayerStatus.Idle
+      ) {
+        console.log("Finished Playing!");
+        this.lastPlayed = oldState.resource.metadata;
+        oldState.resource.metadata.onFinish();
+        this.processQueue();
+      } else if (newState.status === AudioPlayerStatus.Playing) {
+        newState.resource.metadata.onStart();
+        console.log("Playing!");
+      }
+    });
 
-		this.audioPlayer.on('error', (error) => error.resource.metadata.onError(error));
+    this.audioPlayer.on("error", (error) =>
+      error.resource.metadata.onError(error)
+    );
 
-		voiceConnection.subscribe(this.audioPlayer);
-	}
+    voiceConnection.subscribe(this.audioPlayer);
+  }
 
-	enqueue(track) {
-		this.queue.push(track);
-		this.processQueue();
-	}
+  enqueue(track) {
+    this.queue.push(track);
+    this.processQueue();
+  }
 
-	stop() {
-		this.queueLock = true;
-		this.queue = [];
-		this.audioPlayer.stop(true);
-	}
+  stop() {
+    this.queueLock = true;
+    this.queue = [];
+    this.audioPlayer.stop(true);
+  }
 
-	async processQueue() {
-		if (this.queueLock || this.audioPlayer.state.status !== AudioPlayerStatus.Idle) {
-			return;
-		}
+  async processQueue() {
+    if (
+      this.queueLock ||
+      this.audioPlayer.state.status !== AudioPlayerStatus.Idle
+    ) {
+      return;
+    }
 
-		if (this.queue.length === 0) {
-			this.timeoutID = setTimeout(() => {
-				this.voiceConnection.destroy();
-				this.subscriptions.delete(this.guildId);
-			}, 180000); //this should be around 180000 in production (3 minutes), but can be adjusted as needed
-			return;
-		} else {
-			if (this.timeoutID) {
-				clearTimeout(this.timeoutID);
-				this.timeoutID = undefined;
-			}
-		}
+    if (this.queue.length === 0) {
+      this.timeoutID = setTimeout(() => {
+        this.voiceConnection.destroy();
+        this.subscriptions.delete(this.guildId);
+      }, 180000); //this should be around 180000 in production (3 minutes), but can be adjusted as needed
+      return;
+    } else {
+      if (this.timeoutID) {
+        clearTimeout(this.timeoutID);
+        this.timeoutID = undefined;
+      }
+    }
 
-		this.queueLock = true;
+    this.queueLock = true;
 
-		const nextTrack = this.queue.shift();
+    const nextTrack = this.queue.shift();
 
-		try {
-			const resource = await nextTrack.createAudioResource();
-			this.audioPlayer.play(resource);
-			this.queueLock = false;
-		} catch (error) {
-			nextTrack.onError(error);
-			this.queueLock = false;
-			return this.processQueue();
-		}
-	}
-}
+    try {
+      const resource = await nextTrack.createAudioResource();
+      this.audioPlayer.play(resource);
+      this.queueLock = false;
+    } catch (error) {
+      nextTrack.onError(error);
+      this.queueLock = false;
+      return this.processQueue();
+    }
+  }
+};
--- music/track.js
+++ music/track.js
@@ -1,86 +1,97 @@
-const { getInfo, validateURL } = require('ytdl-core');
-const { createAudioResource, demuxProbe } = require('@discordjs/voice');
-const { exec: ytdl } = require('youtube-dl-exec');
-const ytsr = require('ytsr');
+const { getInfo, validateURL } = require("ytdl-core");
+const { createAudioResource, demuxProbe } = require("@discordjs/voice");
+const { exec: ytdl } = require("youtube-dl-exec");
+const ytsr = require("ytsr");
 
-const noop = () => { };
+const noop = () => {};
 
 module.exports = class Track {
+  constructor({ url, title, onStart, onFinish, onError }) {
+    this.url = url;
+    this.title = title;
+    this.onStart = onStart;
+    this.onFinish = onFinish;
+    this.onError = onError;
+  }
 
-    constructor({ url, title, onStart, onFinish, onError }) {
-        this.url = url;
-        this.title = title;
-        this.onStart = onStart;
-        this.onFinish = onFinish;
-        this.onError = onError;
-    }
-
-    createAudioResource() {
-        return new Promise((resolve, reject) => {
-            const process = ytdl(
-                this.url,
-                {
-                    o: '-',
-                    q: '',
-                    f: 'bestaudio[ext=webm+acodec=opus+asr=48000]/bestaudio',
-                    r: '100K',
-                },
-                { stdio: ['ignore', 'pipe', 'ignore'] },
-            );
-            if (!process.stdout) {
-                reject(new Error('No stdout'));
-                return;
-            }
-            const stream = process.stdout;
-            const onError = (error) => {
-                if (!process.killed) process.kill();
-                stream.resume();
-                reject(error);
-            };
-            process
-                .once('spawn', () => {
-                    demuxProbe(stream)
-                        .then((probe) => resolve(createAudioResource(probe.stream, { metadata: this, inputType: probe.type })))
-                        .catch(onError);
+  createAudioResource() {
+    return new Promise((resolve, reject) => {
+      /* use ytdl to get the audio stream for the video requested,
+        note that the "f" flag used to get "bestaudio[ext=webm+acodec=opus+asr=48000]/bestaudio"
+        passed as the value, this for some reason no longer works as ytdl will return nothing if used,
+        instead I now use only "bestaudio" as the value and this seems to work just fine */
+      const process = ytdl(
+        this.url,
+        {
+          o: "-",
+          q: "",
+          f: "bestaudio",
+          r: "100K",
+        },
+        { stdio: ["ignore", "pipe", "ignore"] }
+      );
+      if (!process.stdout) {
+        reject(new Error("No stdout"));
+        return;
+      }
+      const stream = process.stdout;
+      console.log(stream);
+      const onError = (error) => {
+        if (!process.killed) process.kill();
+        stream.resume();
+        reject(error);
+      };
+      process
+        .once("spawn", () => {
+          demuxProbe(stream)
+            .then((probe) =>
+              resolve(
+                createAudioResource(probe.stream, {
+                  metadata: this,
+                  inputType: probe.type,
                 })
-                .catch(onError);
-        });
+              )
+            )
+            .catch(onError);
+        })
+        .catch(onError);
+    });
+  }
+
+  static async from(song, methods) {
+    let info;
+    let url;
+    if (validateURL(song)) {
+      info = await getInfo(song);
+      url = song;
+    } else {
+      const filters = await ytsr.getFilters(song);
+      const filter = filters.get("Type").get("Video");
+      const options = { pages: 1 };
+      const searchResults = await ytsr(filter.url, options);
+      info = await getInfo(searchResults.items[0].url);
+      url = searchResults.items[0].url;
     }
 
-    static async from(song, methods) {
-        let info;
-        let url;
-        if (validateURL(song)) {
-            info = await getInfo(song);
-            url = song;
-        } else {
-            const filters = await ytsr.getFilters(song);
-            const filter = filters.get('Type').get('Video');
-            const options = { pages: 1 };
-            const searchResults = await ytsr(filter.url, options);
-            info = await getInfo(searchResults.items[0].url);
-            url = searchResults.items[0].url;
-        }
+    const wrappedMethods = {
+      onStart() {
+        wrappedMethods.onStart = noop;
+        methods.onStart();
+      },
+      onFinish() {
+        wrappedMethods.onFinish = noop;
+        methods.onFinish();
+      },
+      onError(error) {
+        wrappedMethods.onError = noop;
+        methods.onError(error);
+      },
+    };
 
-        const wrappedMethods = {
-            onStart() {
-                wrappedMethods.onStart = noop;
-                methods.onStart();
-            },
-            onFinish() {
-                wrappedMethods.onFinish = noop;
-                methods.onFinish();
-            },
-            onError(error) {
-                wrappedMethods.onError = noop;
-                methods.onError(error);
-            }
-        };
-
-        return new Track({
-            title: info.videoDetails.title,
-            url,
-            ...wrappedMethods
-        });
-    }
+    return new Track({
+      title: info.videoDetails.title,
+      url,
+      ...wrappedMethods,
+    });
+  }
 };
